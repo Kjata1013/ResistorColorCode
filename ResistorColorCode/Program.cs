c54c878d0954f23164eb912377baed8db141ad4d
Kathy Walker <kjata1013@gmail.com>
2018-01-22 11:47:44 -0500

    hope this works

--- ResistorColorCode/Program.cs
+++ ResistorColorCode/Program.cs
@@ -10,6 +10,32 @@ namespace ResistorColorCode
 	{
 		static void Main(string[] args)
 		{
+			string[] bandColor = new string [10] { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "gray", "white" };
+			int[] multiplier = new int[8];
+			string ohm = "Ω";
+			int sum = 1;
+			multiplier[0] = 1;
+
+			for (int i = 1; i < multiplier.Length; i++)
+			{
+				sum = sum * 10;
+				multiplier[i] = (int)sum;
+			}
+
+			Console.Write("Please enter the first color: ");
+			string firstColor = Console.ReadLine();
+
+			Console.Write("Please enter the second color: ");
+			string secondColor = Console.ReadLine();
+
+			Console.Write("Please enter the color of the multiplier: ");
+			string thridColor = Console.ReadLine();
+
+			int multColorIndex = Array.IndexOf(bandColor, thridColor);
+			int multColorElement = multiplier[multColorIndex];
+
+			Console.WriteLine($"The resistance is {(((Array.IndexOf(bandColor, firstColor))* 10) + Array.IndexOf(bandColor,secondColor)) * multColorElement} {ohm}");
+
 		}
 	}
 }
